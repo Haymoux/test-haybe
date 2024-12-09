778c3c86d9f4049a0e6d0792f1f19379238fb800
AyobamiLad <moriahberry2222@gmail.com>
2024-12-09 08:29:23 +0100

    update on Nav Bar responsiveness

--- frontend/components/NavBar.tsx
+++ frontend/components/NavBar.tsx
@@ -13,7 +13,7 @@ interface NavBarProps {
 export const NavBar: React.FC<NavBarProps> = ({ toggleSidebar, isSidebarOpen }) => {
   return (
     <div className='top-0 p-10 flex w-full items-center justify-between'>
-      <div className='flex items-center w-full gap-16'>
+      <div className='flex items-center w-full gap-16 max-sm:gap-2'>
         {/* Hamburger menu for mobile */}
         <button 
           className='md:hidden mr-4'
@@ -22,16 +22,16 @@ export const NavBar: React.FC<NavBarProps> = ({ toggleSidebar, isSidebarOpen }) 
         >
           {isSidebarOpen ? <FaTimes /> : <FaBars />}
         </button>
-        <h2 className='text-[1.4rem] font-bold'>Hello John</h2>
-        <div className='w-[38%] bg-black h-0.5 hidden md:block'></div>
+        <h2 className='text-[1.4rem] max-sm:text-[1.1rem] font-bold'>Hello John</h2>
+        <div className='w-[38%] bg-black h-0.5 hidden md:block max-sm:hidden'></div>
       </div>
-      <div className='flex gap-4 items-center w-[20vw]'>
+      <div className='flex gap-4 items-center w-[20vw] max-sm:w-[30vw]'>
         <button title='notification'>
           <GoBell width={16} />
         </button>
-        <h3 className='font-semibold'>John Doe</h3>
-        <button title='d' className='flex gap-1 items-center'>
-          <div className='w-12 h-12 bg-slate-100 rounded-full text-white items-center text-2xl justify-center flex border'>
+        <h3 className='font-semibold max-sm:text-sm'>John Doe</h3>
+        <button title='User' className='flex gap-1 items-center'>
+          <div className='w-12 h-12 max-sm:w-10 max-sm:h-10 bg-slate-100 rounded-full text-white items-center text-2xl max-sm:text-lg justify-center flex border'>
             <FaUser />
           </div>
           <div>
